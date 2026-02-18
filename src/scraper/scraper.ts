387b18f107fdc242b997e222d65089377190e754
danygaytan <dnigaytan07@gmail.com>
2026-02-18 02:41:49 -0600

    Updated scraper puppeteer instance args

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -9,9 +9,24 @@ puppeteer.use(StealthPlugin());
 
 export const getUpdatedAssetInfo = async (asset_param: Asset): Promise<(Asset | (string | null))[]> => {
     const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser',
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+        headless: false,
+         args: [
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu",
+                "--disable-extensions",
+                "--disable-background-networking",
+                "--disable-background-timer-throttling",
+                "--disable-client-side-phishing-detection",
+                "--disable-default-apps",
+                "--disable-sync",
+                "--metrics-recording-only",
+                "--no-first-run",
+                "--mute-audio",
+                "--safebrowsing-disable-auto-update",
+                "--window-size=1920,1080"
+            ]
     });
 
     try {
